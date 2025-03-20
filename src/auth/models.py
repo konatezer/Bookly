e0f992217125e7fb475f379b6a47a13cdcb746e9
konatezer <konateze1@gmail.com>
2025-03-20 08:46:00 -0400

    create auth package and user models

---
+++ src/auth/models.py
@@ -0,0 +1,28 @@
+from sqlmodel import SQLModel, Field, Column
+import sqlalchemy.dialects.postgresql as pg
+import uuid
+from datetime import datetime
+
+
+class User(SQLModel, table=True):
+   __tablename__ ='users'
+ 
+   uid: uuid.UUID = Field(
+      sa_column=Column(
+         pg.UUID,
+         nullable=False,
+         primary_key=True,
+         default=uuid.uuid4
+      )
+   )
+   username: str
+   email: str
+   first_name: str
+   last_name: str
+   is_verified: bool = Field(default=False)
+   create_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
+   update_at: datetime = Field(sa_column=Column(pg.TIMESTAMP, default=datetime.now))
+
+
+   def __repr__(self):
+      return f"<user {self.username}>"
